1fd69d02e360f3536f2196d45859c6b42a7e48f5
Emma Gustafsson <emma.i.gustafsson@gmail.com>
2016-12-13 10:32:41 +0100

    solved recovermessage

--- CBC/CBCXor.java
+++ CBC/CBCXor.java
@@ -25,7 +25,7 @@ public class CBCXor {
 
 	/**
 	 * Recover the encrypted message (CBC encrypted with XOR, block size = 12).
-	 * 
+	 *
 	 * @param first_block
 	 *            We know that this is the value of the first block of plain
 	 *            text.
@@ -34,42 +34,28 @@ public class CBCXor {
 	 *            block is 12 bytes long.
 	 */
 	private static String recoverMessage(byte[] first_block, byte[] encrypted) {
-		//MemoryStream mStream = new MemoryStream(encrypted);
-		byte[] decrypt = null;
-		//decrypt = mstream.toByteArray(encrypted);
-		System.out.println("\n In the function");
-        System.out.println("print first byte of encrypted message " +encrypted[0]);
-        System.out.println("print first byte of encrypted message " +(encrypted[1]^encrypted[1]));
-        byte[] msg1 = new byte[108];
-        byte[] msg2 = new byte[108];
-        byte[] pmsg2 = new byte[108];
-        byte[] pmsg1 = new byte[108];
-        //long myArray[] = new long[9];
-        //myArray[0] = (long)encrypted[1];
-        for(int i=0; i<108; i++) {
-        	pmsg2[i] = (byte)i;
-        }
-        //encrypting pmsg2 considering same IV and key
-        for(int i=0; i<108; i++) {
-            if(i<12) {//keeping the first block that is IV same so (K + IV) remains same as given encrypted message
-                msg2[i] = encrypted[i];
-                //System.out.println(" "+msg2[i]);
-            }
-            else {//computing next byte from previous byte
-                msg2[i] = (byte)((int)msg2[i-12]^(int)pmsg2[i]);
-            }
-        	
-        }
-        //reconstructing the message 1 by XORing both the cipher text
-        for(int i=11; i<108; i++) {
-        	msg1[i] = (byte)((int)msg2[i]^(int)encrypted[i]);
-            pmsg1[i] = (byte)((int)msg1[i]^(int)pmsg2[i]);
-            //System.out.println(pmsg1[i]);
-           
-        }
-        
+		byte [] iv = new byte[12];
+		byte[] decrypted = new byte [encrypted.length-1];
+		byte[] msg1 = new byte [12];
+		byte[] msg2 = new byte [12];
+		byte[] k = new byte[12];
+		for(int i = 0; i<iv.length; i++){ // IV
+				iv[i] = encrypted[i];
+				k[i] = (byte)(iv[i]^encrypted[i+12]^first_block[i]);
+				msg1[i] = (byte)(k[i]^first_block[i]^iv[i]);
 
- 
-		return new String(encrypted);
+		}
+		int j = 0;
+		for(int i=24; i<encrypted.length-3; i++){ // Added "-3" because of the padding
+			decrypted[i-24] = (byte)(k[j]^msg1[j]^encrypted[i]);
+			msg2[j] = encrypted[i]; // save previous C in msg2
+			if(j==11){
+				j=0;
+				msg1=msg2;
+			}else{
+				j++;
+			}
+		}
+		return new String(decrypted);
 	}
 }
